3268d775b7b00775ca886345ae16ea8e88b621b5
06kaylee <06.kaylee@gmail.com>
2020-07-15 13:07:52 -0700

    Expand objects key

--- mitre_script.py
+++ mitre_script.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 output_path = Path('d:\Splunk_Inputs\MITREDefs\enterprise-attack.json')
 
+
 with urllib.request.urlopen("https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json") as url:
     data = json.loads(url.read().decode())
+    final_data = data["objects"]
     with open(output_path, "w") as output:
-        json.dump(data, output, indent = 4)
+        json.dump(final_data, output, indent = 4)
 
     
 
